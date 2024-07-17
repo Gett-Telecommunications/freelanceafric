0d6192eb01d7cee0c13607b11ffb1d6a055786da
Jude Otenyo <judeotenyo@gmail.com>
2024-07-17 20:18:29 +0300

    feat(search): :sparkles: Created a new feature for search

---
+++ libs/search/ng/ui/src/lib/components/search-field.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-search-field',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-field.component.html',
+  styleUrl: './search-field.component.scss',
+})
+export class SearchFieldComponent {}
---
+++ libs/search/shared/src/lib/interfaces/search-result.interface.ts
@@ -0,0 +1,5 @@
+export interface I_SearchResult {
+  id: string;
+  label: string;
+  link: string;
+}
