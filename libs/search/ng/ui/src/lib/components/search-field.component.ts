6d586b334482426c80912edbf303bdd1dab393cc
Jude Otenyo <judeotenyo@gmail.com>
2024-07-18 12:21:36 +0300

    feat(search): :sparkles: Created the search field

--- libs/search/ng/ui/src/lib/components/search-field.component.ts
+++ libs/search/ng/ui/src/lib/components/search-field.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'lib-search-field',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+  ],
   templateUrl: './search-field.component.html',
   styleUrl: './search-field.component.scss',
 })
-export class SearchFieldComponent {}
+export class SearchFieldComponent {
+  showFilters = input<boolean>(false);
+  mode = input.required<'full' | 'compact'>();
+  searchForm = new FormGroup({
+    searchTerm: new FormControl(''),
+  });
+  filtersForm = new FormGroup({
+    typeFilter: new FormControl('all'),
+  });
+}
